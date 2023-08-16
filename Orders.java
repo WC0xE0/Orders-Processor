c4f456e546b51aed831b3a082eb39541c783fcec
WC0xE0 <107419968+WC0xE0@users.noreply.github.com>
2023-08-16 08:50:18 -0400

    Create Orders.java

---
+++ Orders.java
@@ -0,0 +1,33 @@
+package processor;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeSet;
+
+public class Orders {
+	private Collection<ClientData> ordersCollection;
+	private ClientData summary;
+	
+	public Orders() {
+		// shared data structure:
+		ordersCollection = new TreeSet<ClientData>();
+		summary = new ClientData();
+	}
+	
+	public Collection<ClientData> getOrders() {
+		return ordersCollection;
+	}
+	
+	public void add(ClientData order) {
+		synchronized(ordersCollection) {
+			ordersCollection.add(order);
+		}
+		synchronized(summary) {
+			summary.merge(order);
+		}
+	}
+
+	public String getSummary(Map<String, ItemData> itemsDatabase) {
+		return summary.toSummaryString(itemsDatabase);
+	}	
+}
